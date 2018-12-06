9e835dea8e7038874e07d88c253750fac5c26828
Mr Scary <ronwazny@gmail.com>
2018-12-05 20:55:41 -0500

    Second Pile

--- assests/javaScript/java.js
+++ assests/javaScript/java.js
@@ -11,29 +11,34 @@ firebase.initializeApp(config);
 // Create a variable to reference the database
 var database = firebase.database();
 
+// Initial Values
+var trainName = "";
+var trainDestination = "";
+var firstTrain = "";
+var trainFrequency = "";
+
 // 2. Button for adding Trains
 $("#add-train-btn").on("click", function(event) {
   //prevent page from refreshing
   event.preventDefault();
 
   // Grabs Train input
-  var trainName = $("#train-name-input")
+  trainName = $("#train-name-input")
     .val()
     .trim();
-  var trainDestination = $("#destination-input")
+  trainDestination = $("#destination-input")
     .val()
     .trim();
   // Using Military Time
-  var firstTrain = moment(
+  firstTrain = moment(
     $("#frequency-input")
       .val()
       .trim(),
     "DD/MM/YY"
   ).format("X");
-  var trainFrequency = $("#frequency-min-input")
+  trainFrequency = $("#frequency-min-input")
     .val()
     .trim();
-  console.log(trainFrequency);
 
   database.ref().set({
     trainName: name,
@@ -41,19 +46,10 @@ $("#add-train-btn").on("click", function(event) {
     firstTrain: firstTrain,
     trainFrequency: trainFrequency
   });
+});
 
-  // Uploads train data to the database
-  database.ref().push(trainName);
-
-  // Firebase watcher + initial loader HINT: .on("value")
-  database.ref().on("value", function(snapshot) {});
-
-  // Store everything into a variable.
-  var trainName = snapshot.val().name;
-  var trainDestination = snapshot.val().destination;
-  var firstTrain = snapshot.val().train;
-  var trainFrequency = snapshot.val().frequency;
-
+// Firebase watcher + initial loader HINT: .on("value")
+database.ref().on("value", function(snapshot) {
   // Log everything that's coming out of snapshot
   console.log(snapshot.val());
   console.log(snapshot.val().name);
@@ -62,10 +58,19 @@ $("#add-train-btn").on("click", function(event) {
   console.log(snapshot.val().frequency);
 
   // Change the HTML to reflect entered
-  $("#train-name-display").text(snapshot.val().name);
-  $("#destination-display").text(snapshot.val().destination);
-  $("#frequency-display").text(snapshot.val().train);
-  $("#frequency-min-display").text(snapshot.val().frequency);
+  $("#train-name-display").html(snapshot.val().name);
+  $("#destination-display").html(snapshot.val().destination);
+  $("#frequency-display").html(snapshot.val().train);
+  $("#frequency-min-display").html(snapshot.val().frequency);
+
+  // Store everything into a variable for next Train.
+  var trainName = snapshot.val().name;
+  var trainDestination = snapshot.val().destination;
+  var firstTrain = snapshot.val().train;
+  var trainFrequency = snapshot.val().frequency;
+
+  // Uploads train data to the database
+  database.ref().push(trainName);
 
   // Logs everything to console
   console.log(trainName.name);
